4d43b574f3e167b8177ada47144fd4273660f767
ytison <yannick.tison@gmail.com>
2023-07-06 09:43:43 +0200

    force apt maven install

--- deploy-java-app-in-docker/background.sh
+++ deploy-java-app-in-docker/background.sh
@@ -4,6 +4,6 @@ set -x # to test stderr output in /var/log/killercoda
 
 echo starting... # to test stdout output in /var/log/killercoda
 
-apt install maven
+apt install maven -f
 
 touch /tmp/finished
