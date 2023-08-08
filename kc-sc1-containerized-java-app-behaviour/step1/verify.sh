fcb2d41f9594a094b1e589432059f33f1f22c114
ytison <yannick.tison@gmail.com>
2023-08-08 17:32:31 +0200

    check that docker image is pulled

--- kc-sc1-containerized-java-app-behaviour/step1/verify.sh
+++ kc-sc1-containerized-java-app-behaviour/step1/verify.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
 app_jar="$HOME/killercoda-artifacts/sc1/java-mem-block-reserver-j17-latest.jar"
-# Vérifier si le fichier readme.md existe
+
+# Check if the app_jar file exists
 if [[ -f "$app_jar" ]]; then
-  echo "Le fichier $app_jar existe."
+  echo "The file $app_jar exists."
 else
-  echo "Le fichier $app_jar n'existe pas."
-  exit 1  # Code de sortie 1 pour indiquer une erreur
+  echo "The file $app_jar does not exist."
+  exit 1  # Exit code 1 to indicate an error
+fi
+
+# Check if the Docker image exists locally
+if docker images | grep -q "ghcr.io/techlabfdj/go-mem-block-reserver"; then
+  echo "The Docker image ghcr.io/techlabfdj/go-mem-block-reserver exists locally."
+else
+  echo "The Docker image ghcr.io/techlabfdj/go-mem-block-reserver does not exist locally."
+  exit 2  # Exit code 2 to indicate another error
 fi
