a06c82eb0f9c460beef0ee25ecf68522f8c466e2
ytison <yannick.tison@gmail.com>
2023-08-08 15:57:45 +0200

    add jq installation

--- kc-sc1-containerized-java-app-behaviour/background.sh
+++ kc-sc1-containerized-java-app-behaviour/background.sh
@@ -7,5 +7,6 @@ echo starting... # to test stdout output in /var/log/killercoda
 git clone https://github.com/techlabfdj/killercoda-artifacts.git
 
 apt install openjdk-17-jre-headless -y
+apt install jq -y
 
 touch /tmp/finished
