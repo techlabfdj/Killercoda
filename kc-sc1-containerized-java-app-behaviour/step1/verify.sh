f7db1ffed3e1027ac5b6ba9db570e35bc307e791
ytison <yannick.tison@gmail.com>
2023-08-29 10:29:51 +0200

    fix step1 verify

--- kc-sc1-containerized-java-app-behaviour/step1/verify.sh
+++ kc-sc1-containerized-java-app-behaviour/step1/verify.sh
@@ -11,9 +11,9 @@ else
 fi
 
 # Check if the Docker image exists locally
-if docker images | grep -q "ghcr.io/techlabfdj/go-mem-block-reserver"; then
-  echo "The Docker image ghcr.io/techlabfdj/go-mem-block-reserver exists locally."
+if docker images | grep -q "ghcr.io/techlabfdj/java-mem-block-reserver"; then
+  echo "The Docker image ghcr.io/techlabfdj/java-mem-block-reserver exists locally."
 else
-  echo "The Docker image ghcr.io/techlabfdj/go-mem-block-reserver does not exist locally."
+  echo "The Docker image ghcr.io/techlabfdj/java-mem-block-reserver does not exist locally."
   exit 2  # Exit code 2 to indicate another error
 fi
