18bf010d9d060a315a2ee643bec0c3a2035fbe69
Yannick Tison <ytison@lfdj.com>
2023-10-02 14:19:24 +0200

    sc4 step 1 verify

--- kc-sc4-golang-training/step1/verify.sh
+++ kc-sc4-golang-training/step1/verify.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-# check if the gitlab container exists
-exit 0
+# Check if Go is installed
+if command -v go &> /dev/null; then
+    # Get the Go version
+    go_version=$(go version)
+
+    # Check if the Go version matches "go1.21" or higher
+    if [[ $go_version == *"go1.21"* ]]; then
+        echo "Go 1.21 or higher is installed: $go_version"
+        exit 0 # Success, return 0
+    else
+        echo "Go is installed, but it's not version 1.21 or higher: $go_version"
+        exit 1 # Failure, return 1
+    fi
+else
+    echo "Go is not installed on this system."
+    exit 1 # Failure, return 1
+fi
