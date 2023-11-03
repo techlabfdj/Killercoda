77e024e940f50839c8b1ea787cc1491b33eb216b
Yannick Tison <ytison@lfdj.com>
2023-11-03 10:44:42 +0100

    sc4 step1 update verify

--- kc-sc4-golang-training/step1/verify.sh
+++ kc-sc4-golang-training/step1/verify.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Define the paths to the directories
+GO_SRC="$HOME/go/src"
+GO_BIN="$HOME/go/bin"
+
+# Check if the $HOME/go/src directory exists
+if [ -d "$GO_SRC" ]; then
+    echo "The directory $GO_SRC exists."
+else
+    echo "The directory $GO_SRC does not exist."
+    exit 1 # Failure, return 1
+fi
+
+# Check if the $HOME/go/bin directory exists
+if [ -d "$GO_BIN" ]; then
+    echo "The directory $GO_BIN exists."
+else
+    echo "The directory $GO_BIN does not exist."
+    exit 1 # Failure, return 1
+fi
+
+
 # Check if Go is installed
 if command -v go &> /dev/null; then
     # Get the Go version
@@ -8,7 +29,6 @@ if command -v go &> /dev/null; then
     # Check if the Go version matches "go1.21" or higher
     if [[ $go_version == *"go1.21"* ]]; then
         echo "Go 1.21 or higher is installed: $go_version"
-        exit 0 # Success, return 0
     else
         echo "Go is installed, but it's not version 1.21 or higher: $go_version"
         exit 1 # Failure, return 1
@@ -17,3 +37,6 @@ else
     echo "Go is not installed on this system."
     exit 1 # Failure, return 1
 fi
+
+# Exit with the return code indicated by status
+exit 0
